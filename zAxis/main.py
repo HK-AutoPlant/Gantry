b0601a892143c9f8e92c2ca7fb72dd209206d21d
Philip Hamilton <Philip@Philips-MacBook-Pro.local>
2020-09-21 15:58:17 +0200

    Added code for the zAxis

---
+++ zAxis/main.py
@@ -0,0 +1,21 @@
+from Class.MotorClass import stepperMotor
+import socket
+
+TCP_IP = '000.000.000.000';
+TCP_PORT = 0000;
+BUFFER_SIZE = 128;
+
+zAxisMotor = stepperMotor(3, 5, 7, 11);
+zAxisMotor.Home();
+
+
+while(True):
+    msg = 'z200'; #Read the message:
+    if(msg =='Home'):
+        zAxisMotor.Home();
+    elif(msg[0] == 'z'):
+        zAxisMotor.move(int(msg[1:]));
+    elif(msg == 'Auto'):
+        print("Do something Autonomous!")
+    else:
+        print("Nothing to do..");
