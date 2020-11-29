61f589967adf52a1943e22c30e76f66b8906d57f
Philip <philip.h.hamilton@gmail.com>
2020-11-29 11:14:02 +0100

    File for those who use Arduino IDE

---
+++ zAxis/Arduino/StepperMotorClass.cpp
@@ -0,0 +1,140 @@
+#include "Arduino.h"
+#include "SteppermotorClass.h"
+
+#define CLOCKWISE 1
+#define COUNTERCLOCKWISE 0
+
+
+stepperMotor::stepperMotor(uint8_t stepPin, uint8_t dirPin, uint8_t enablePin, uint8_t limitSwitchPin)
+: _A4988(stepPin, dirPin, enablePin), _limitSwitch(limitSwitchPin){}
+
+void stepperMotor::initialize()
+{
+  _A4988.initialize();
+  _limitSwitch.initialize();
+
+  _mmPerStep = (float)mmPerRev/stepsPerRev;
+}
+
+void stepperMotor::moveUp()
+{
+  moveDistance(-((int)_currentPosition - limitSwitchOffset));
+}
+
+void stepperMotor::moveDown()
+{
+  moveDistance(maxDistance - (int)_currentPosition);
+}
+
+void stepperMotor::moveTo(float position)
+{
+  moveDistance(position - _currentPosition);
+}
+
+void stepperMotor::moveDistance(int distance)
+{
+  if(!_initialHoming)
+  {
+    Serial.println("The Home is not homed!");
+    return;
+  }
+
+  distance >= 0 ? _moveCCW() : _moveCW();
+
+  _numberOfSteps = _distanceToSteps(abs(distance));
+
+  _A4988.enableMotor(true);
+  for(int i = 0; i < _numberOfSteps; i++)
+  {
+    _updateCurrentPosition(distance);
+      if(_withinBoundaries())
+      {
+        _A4988.step();
+      }
+      else
+      {
+        break;
+      }
+  }
+
+  _A4988.enableMotor(false);
+}
+
+void stepperMotor::home()
+{
+  _initialHoming = 1;
+  _moveCW();
+  _A4988.enableMotor(true);
+  while(_limitSwitch.isPressed() == false)
+    {
+      _A4988.step();
+    }
+  _A4988.enableMotor(false);
+  _currentPosition = 0;
+  delay(500);
+
+  moveDistance(limitSwitchOffset);
+}
+
+
+void stepperMotor::_updateCurrentPosition(int distance)
+{
+   distance >= 0 ? _currentPosition+= _mmPerStep : _currentPosition -= _mmPerStep;
+}
+
+void stepperMotor::holdingTorque(bool state)
+{
+  state == true ? _A4988.enableMotor(true) : _A4988.enableMotor(false);
+}
+
+int stepperMotor::_distanceToSteps(int distance)
+{
+  return (int)distance/_mmPerStep;
+}
+
+bool stepperMotor::_withinBoundaries()
+{
+  if(_currentPosition >= (float)maxDistance)
+  {
+    _currentPosition = (float)maxDistance; // Resolves an issue that the machine takes one step to much
+    return 0;
+  }else if(_limitSwitch.isPressed() && _dir == CLOCKWISE)
+  {
+    _currentPosition = 0.0;
+    return 0;
+  }
+  else{
+    return 1;
+  }
+}
+
+void stepperMotor::status()
+{
+  Serial.println("----- Current Status -----");
+  Serial.print("Current Position:");
+  Serial.print("\t");
+  Serial.print("\t");
+  Serial.print(_currentPosition);
+  Serial.println(" mm.");
+  Serial.print("Limit switch Offset:");
+  Serial.print("\t");
+  Serial.print("\t");
+  Serial.print(limitSwitchOffset);
+  Serial.println(" mm.");
+  Serial.print("Maximum allowed position:");
+  Serial.print("\t");
+  Serial.print(maxDistance);
+  Serial.println(" mm.");
+}
+
+void stepperMotor::_moveCW()
+{
+  _A4988.direction(CLOCKWISE);
+  _dir = CLOCKWISE;
+}
+
+void stepperMotor::_moveCCW()
+{
+  _A4988.direction(COUNTERCLOCKWISE);
+  _dir = COUNTERCLOCKWISE;
+}
---
+++ zAxis/Arduino/zAxis.ino
@@ -0,0 +1,149 @@
+#include "StepperMotorClass.h"
+#include "limitSwitchClass.h"
+
+#define BAUD_RATE 115200
+#define COMMAND_COMPLETED 1
+#define COMMAND_NOT_COMPLETED 0
+
+
+uint8_t zAxisDirPin    = 9;
+uint8_t zAxisStepPin   = 10;
+uint8_t zAxisEnablePin = 11;
+uint8_t zAxisLimitSwitchPin = 14; // Same as A0!
+
+uint8_t gripperDirPin    = 5;
+uint8_t gripperStepPin   = 6;
+uint8_t gripperEnablePin = 7;
+uint8_t gripperAxisLimitSwitchPin = 18; // Same as A4!
+
+uint8_t soilSensorPin1 = 15;
+uint8_t soilSensorPin2 = 16;
+uint8_t soilSensorPin3 = 17;
+uint8_t soilSensorPin4 = 20;
+uint8_t soilSensorPin5 = 21;
+
+
+stepperMotor zAxis(zAxisStepPin, zAxisDirPin, zAxisEnablePin, zAxisLimitSwitchPin);
+stepperMotor Gripper(gripperStepPin, gripperDirPin, gripperEnablePin, gripperAxisLimitSwitchPin);
+
+limitSwitch soilSensor1(soilSensorPin1);
+limitSwitch soilSensor2(soilSensorPin2);
+limitSwitch soilSensor3(soilSensorPin3);
+limitSwitch soilSensor4(soilSensorPin4);
+limitSwitch soilSensor5(soilSensorPin5);
+
+
+String msg;
+int distance;
+int position;
+
+int parseMessage(String msg);
+void detectSoil();
+
+void setup()
+{
+  Serial.begin(BAUD_RATE);
+  zAxis.maxDistance = 150;
+  zAxis.mmPerRev = 6;
+  zAxis.initialize();
+
+  Gripper.maxDistance = 10;
+  Gripper.limitSwitchOffset = 0;
+  Gripper.stepsPerRev = 200;
+  Gripper.initialize();
+
+  soilSensor1.initialize();
+  soilSensor2.initialize();
+  soilSensor3.initialize();
+  soilSensor4.initialize();
+  soilSensor5.initialize();
+
+}
+
+void loop() {
+  // Recieve Message:
+  // If message Recieved:
+
+  while(Serial.available() > 0)
+  {
+    Serial.println(COMMAND_NOT_COMPLETED);
+    msg = Serial.readString();
+
+    switch (msg[0]) {
+      case 'H': //Home zAxis
+        zAxis.home();
+      break;
+
+      case 'h': //Home Gripper
+        Gripper.home();
+      break;
+
+      case 'z': //Move zAxis
+        distance = parseMessage(msg);
+        zAxis.moveDistance(distance);
+      break;
+
+      case 'g': //Move Gripper
+        distance = parseMessage(msg);
+        Gripper.moveDistance(distance);
+      break;
+
+      case 'D': //Move down zAxis
+        zAxis.moveDown();
+      break;
+      case 'd': //Move down Gripper
+        Gripper.moveDown();
+      break;
+
+      case 'P':
+        position = parseMessage(msg);
+        zAxis.moveTo(position);
+      break;
+
+      case 'p':
+        position = parseMessage(msg);
+        Gripper.moveTo(position);
+      break;
+
+      case 'c': //Check Soil Sesors
+        detectSoil();
+      break;
+
+      case 's': //Prints out the current status
+        zAxis.status();
+        Gripper.status();
+      break;
+
+      default:
+        //Serial.println("Message not understood");
+      break;
+    }
+    Serial.println(COMMAND_COMPLETED);
+  }
+}
+
+int parseMessage(String msg)
+{
+  bool negativeNumber = 0;
+
+  if(msg[1] == '-')
+  {
+    msg.remove(0, 2);
+    negativeNumber = 1;
+  }
+  else
+  {
+    msg.remove(0, 1);
+  }
+
+  return negativeNumber == 1 ? (-1)*msg.toInt() : msg.toInt();
+}
+
+void detectSoil()
+{
+  Serial.print(soilSensor1.isPressed());
+  Serial.print(soilSensor2.isPressed());
+  Serial.print(soilSensor3.isPressed());
+  Serial.print(soilSensor4.isPressed());
+  Serial.print(soilSensor5.isPressed());
+}
